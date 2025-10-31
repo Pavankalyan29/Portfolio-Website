006346d8d565fe21f338b764ac8226d82b367f9c
Pavankalyan29 <pavankalyan.pentam29@gmail.com>
2025-10-31 15:53:52 +0530

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "ecr_repo_name" {
@@ -19,7 +19,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "SSH key pair name (optional)"
   type        = string
-  default     = ""
+  default     = "jenkins-key"
 }
 
 variable "allowed_cidr" {
