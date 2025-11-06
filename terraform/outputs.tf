4ee81614925b5efbb78af50ebbe50ab02f637026
Pavankalyan29 <pavankalyan.pentam29@gmail.com>
2025-11-06 14:58:48 +0530

    updated outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "ecr_repo_url" {
-  description = "ECR Repository URL"
-  value       = aws_ecr_repository.app.repository_url
-}
-
 output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
